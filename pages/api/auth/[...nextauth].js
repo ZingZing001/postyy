a39997ecb91580564d7a442ecb6375ddc8bf3725
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-08-28 16:48:39 +1200

    chore: Update NextAuth configuration, Prisma adapter, and dependencies

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from '../../../prisma/client'
 
 const adapter = PrismaAdapter(prisma)
@@ -15,13 +15,4 @@ export const authOptions = {
   ],
 }
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.AUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-})
+export default NextAuth(authOptions)
