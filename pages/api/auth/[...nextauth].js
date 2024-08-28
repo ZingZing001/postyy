3229207acd0a655029f759491a87549cd1b007fd
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-08-28 16:27:39 +1200

    chore: Update Login button styling

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from '../../../prisma/client'
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
