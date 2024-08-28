62f26cae14b15776887b3a961248a891945b46f6
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-08-28 18:07:58 +1200

    feat: Improve character count display in AddPost component

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -16,7 +16,12 @@ export default function CreatePost() {
           className="p-4 text-lg rounded-md my-2 bg-gray-200"
         ></textarea>
       </div>
-      <div>
+      <div className="flex items-center justify-between gap-2">
+        <p
+          className={`font-bold text-sm ${
+            title.length > 300 ? "text-red-700" : "text-grey-700"
+          }`}
+        >{`${title.length}/300`}</p>
         <button
           disabled={isDisabled}
           className="text-white text-sm px-6 py-2 rounded-xl bg-teal-600 disabled:opacity-25"
