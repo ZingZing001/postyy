1e5e1ff6e555ef7508e926f9a20e12213ea6792e
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-08-28 18:04:58 +1200

    feat: Add lazy loading for images in AddPost component

--- app/components/AddPost.tsx
+++ app/components/AddPost.tsx
@@ -1,19 +1,30 @@
-'use client'
-import { useState } from "react"
-import React from "react"
+"use client";
+import { useState } from "react";
+import React from "react";
 
 export default function CreatePost() {
-  const [title, setTitle] = useState("")
+  const [title, setTitle] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
   return (
-    <form>
-      <div>
-        <textarea 
-        onChange = {(e) => setTitle(e.target.value)}
-        name="title"
-        value = {title}
-        placeholder="What's on your mind?"
+    <form className="bg-white my-8 p-8 rounded-md">
+      <div className="flex flex-col my-4">
+        <textarea
+          onChange={(e) => setTitle(e.target.value)}
+          name="title"
+          value={title}
+          placeholder="What's on your mind?"
+          className="p-4 text-lg rounded-md my-2 bg-gray-200"
         ></textarea>
       </div>
+      <div>
+        <button
+          disabled={isDisabled}
+          className="text-white text-sm px-6 py-2 rounded-xl bg-teal-600 disabled:opacity-25"
+          type="submit"
+        >
+          Create a Post
+        </button>
+      </div>
     </form>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import React from "react";
+import AddPost from "./components/AddPost";
+
 export default function Home() {
   return (
     <main>
       <h1>Hello test</h1>
+      <AddPost />
     </main>
   );
 }
