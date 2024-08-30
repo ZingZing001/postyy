9e0a90028b8e0d252363bdfe92c373a6885955ef
ZingZing_ <rzha972@aucklanduni.ac.nz>
2024-08-30 22:09:36 +1200

    feat: Add lazy loading for images in MyPosts component

--- app/components/AddComment.tsx
+++ app/components/AddComment.tsx
@@ -1,15 +1,53 @@
 "use client";
 
 import { useState } from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
 
-export default function AddComment({ id }) {
+type PostProps = {
+  id?: string;
+};
+
+type Comment = {
+  postId?: string;
+  title: string;
+};
+
+export default function AddComment({ id }: PostProps) {
   const [title, setTitle] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
+  const queryClient = useQueryClient();
+  let commentToastID: string;
+
+  const { mutate } = useMutation({
+    mutationFn: (data: Comment) =>
+      axios.post("/api/posts/addComment", { data }),
+    onSuccess: (data) => {
+      setTitle("");
+      setIsDisabled(false);
+      toast.success("Comment added successfully 🎉", { id: commentToastID });
+    },
+    onError: (error) => {
+      setIsDisabled(false);
+      if (error instanceof AxiosError) {
+        toast.error(error?.response?.data.message, { id: commentToastID });
+      }
+    },
+  });
+
+  const submitComment = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
+    commentToastID = toast.loading("Adding your comment...", {
+      id: commentToastID,
+      duration: 1000,
+    });
+    mutate({ title, postId: id });
+  };
+
   return (
-    <form className="my-8">
+    <form onSubmit={submitComment} className="my-8">
       <h3>Add a comment</h3>
       <div className="flex flex-col my-2">
         <input
